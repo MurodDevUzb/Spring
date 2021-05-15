f20a5e9ae00f5703cab7ce71c9b048776eaf2f2a
MurodDevUzb <murodabduraxmonov7@gmail.com>
2021-05-15 13:12:16 -0700

    Spring security with roles

---
+++ SpringSecurityInMemoryWithRoles/src/main/java/uz/dev/security/config/SecurityConfig.java
@@ -0,0 +1,56 @@
+package uz.dev.security.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import uz.dev.security.model.Role;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/").permitAll()
+                .antMatchers(HttpMethod.GET , "/api/**").hasAnyRole(Role.ADMIN.name(), Role.USER.name())
+                .antMatchers(HttpMethod.POST, "/api/**").hasRole(Role.ADMIN.name())
+                .antMatchers(HttpMethod.DELETE, "/api/**").hasRole(Role.ADMIN.name())
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic();
+    }
+
+    @Bean
+    @Override
+    protected UserDetailsService userDetailsService() {
+        return new InMemoryUserDetailsManager(
+                User.builder()
+                    .username("user")
+                    .password(bCryptPasswordEncoder().encode("user"))
+                        .roles(Role.USER.name())
+                    .build(),
+
+                User.builder()
+                    .username("admin")
+                    .password(bCryptPasswordEncoder().encode("admin"))
+                        .roles(Role.ADMIN.name())
+                    .build()
+        );
+    }
+
+    @Bean
+    protected BCryptPasswordEncoder bCryptPasswordEncoder(){
+        return new BCryptPasswordEncoder(12);
+    }
+}
---
+++ SpringSecurityInMemoryWithRoles/src/main/java/uz/dev/security/model/Role.java
@@ -0,0 +1,6 @@
+package uz.dev.security.model;
+
+public enum Role {
+    USER,
+    ADMIN;
+}
