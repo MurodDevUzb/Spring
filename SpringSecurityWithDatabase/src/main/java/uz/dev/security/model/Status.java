ff429f970358b0eb1228175b33994dae144bb0cc
MurodDevUzb <murodabduraxmonov7@gmail.com>
2021-05-18 10:21:32 -0700

    Spring security with database

---
+++ SpringSecurityWithDatabase/src/main/java/uz/dev/security/model/Status.java
@@ -0,0 +1,5 @@
+package uz.dev.security.model;
+
+public enum Status {
+    ACTIVE,BANNED;
+}
---
+++ SpringSecurityWithDatabase/src/main/java/uz/dev/security/security/UserDetailsServiceImpl.java
@@ -0,0 +1,28 @@
+package uz.dev.security.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import uz.dev.security.model.User;
+import uz.dev.security.repository.UserRepository;
+
+@Service("UserDetailsServiceImpl")
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserDetailsServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email).orElseThrow(()->
+                new UsernameNotFoundException("User doesn't exists"));
+
+        return SecurityUser.fromUser(user);
+    }
+}
