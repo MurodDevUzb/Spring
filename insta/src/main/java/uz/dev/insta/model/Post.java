284cf5fa1e608e06c76ecfcf16ef941417c17c70
MurodDevUzb <murodabduraxmonov7@gmail.com>
2021-05-21 11:23:48 -0700

    Begining of the project

---
+++ insta/src/main/java/uz/dev/insta/model/Post.java
@@ -0,0 +1,4 @@
+package uz.dev.insta.model;
+
+public class Post {
+}
---
+++ insta/src/test/java/uz/dev/insta/InstaApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.dev.insta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
