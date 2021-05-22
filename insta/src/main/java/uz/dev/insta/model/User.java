e825ebbd54d0fd07109f8849a059465799bf54a0
MurodDevUzb <murodabduraxmonov7@gmail.com>
2021-05-22 15:21:38 -0700

    Add models

--- insta/src/main/java/uz/dev/insta/model/User.java
+++ insta/src/main/java/uz/dev/insta/model/User.java
@@ -1,4 +1,55 @@
 package uz.dev.insta.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import uz.dev.insta.model.enums.Role;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.*;
+
+@Data
+@Entity
+@Table(name = "user")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String name;
+    @Column(nullable = false)
+    private String lastname;
+    @Column(unique = true,updatable = false)
+    private String username;
+    @Column(unique = true)
+    private String email;
+    @Column(columnDefinition = "text")
+    private String bio;
+    @Column(length = 3000)
+    private String password;
+
+    @ElementCollection(targetClass = Role.class)
+    @CollectionTable(name = "user_role",
+    joinColumns = @JoinColumn(name = "user_id"))
+    private Set<Role> role = new HashSet<>();
+
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY,mappedBy = "user",orphanRemoval = true)
+    private List<Post> posts = new ArrayList<>();
+
+    @JsonFormat(pattern = "yy-mm-dd HH:mm:ss")
+    @Column(updatable = false)
+    private LocalDateTime createdDate;
+
+
+    @Transient
+    private Collection<? extends GrantedAuthority> authorities;
+
+    @PrePersist
+    protected void onCreate(){
+        this.createdDate = LocalDateTime.now();
+    }
+
+
 }
